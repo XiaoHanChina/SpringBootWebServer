79260e95ab2eda97b4f5db343ce59abe486a5966
XiaoHanChina <xiaohanshaobing@163.com>
2017-10-23 20:25:56 +0800

    log配置

--- src/main/java/com/hanxs/springboot/controller/WelcomeController.java
+++ src/main/java/com/hanxs/springboot/controller/WelcomeController.java
@@ -1,13 +1,18 @@
 package com.hanxs.springboot.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
-	
-	@RequestMapping("/")//Get any visitation by this route, no matter GET, POST, DELETE, etc.
+	private static final Logger logger = LoggerFactory.getLogger(WelcomeController.class);
+
+	@RequestMapping("/") // Get any visitation by this route, no matter GET,
+							// POST, DELETE, etc.
 	public String hello() {
+		logger.info("Get one visitor！");// 这里支持常见log的debug/info/warn/error等级别
 		return "Welcome to my website!";
 	}
 }
